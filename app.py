5d41062c680d7360970ae9e835e384f288ae8633
Samarp Jain <107236103+Samarp1001@users.noreply.github.com>
2024-11-23 11:39:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "hello, from new edit"
+    return "hello, this the new version"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=80)
